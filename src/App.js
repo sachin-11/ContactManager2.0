a20e2aa245bb24de6b2b0417e03cbc28dd486fe0
sachin singh <sachin.singh@antino.io>
2020-04-19 23:16:59 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div class='App'>
+        <Header branding='Contact Manager' />
+        <Contact
+          name='sachin singh'
+          email='rajeshsachin786@gmail.com'
+          phone='9834573812'
+        />
+        <Contact
+          name='rahul singh'
+          email='rahulsingh786@gmail.com'
+          phone='77167681897'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
