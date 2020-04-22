7cdc5a1f718d21e1787657963609f14edb7f41c5
sachin singh <sachin.singh@antino.io>
2020-04-22 23:49:03 +0530

    Updates

---
+++ precache-manifest.b6175ed63a88801fe627e93f7acbb2c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe014a0b4eeba29dcc717dbd2d8a9255",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "118f9a6dd543c15fbd9e",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4f4506ee4fa2142d86ce",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "118f9a6dd543c15fbd9e",
+    "url": "/contactmanager/static/js/2.781008fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.781008fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4506ee4fa2142d86ce",
+    "url": "/contactmanager/static/js/main.bb150a1a.chunk.js"
+  },
+  {
+    "revision": "9d295c85adef88e5266e",
+    "url": "/contactmanager/static/js/runtime-main.8911d98c.js"
+  }
+]);
